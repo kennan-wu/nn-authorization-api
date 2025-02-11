843104ff139d06c91001a06e078fc9cde56f07a2
nyook941 <73042201+nyook941@users.noreply.github.com>
2025-02-11 16:10:26 -0600

    cookie modifications

--- src/main/java/com/kennan/mp3player/mp3_player_api/configs/JwtAuthenticationFilter.java
+++ src/main/java/com/kennan/mp3player/mp3_player_api/configs/JwtAuthenticationFilter.java
@@ -22,17 +22,16 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-public class JwtAuthenticationFilter extends OncePerRequestFilter{
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final HandlerExceptionResolver handlerExceptionResolver;
 
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
 
     public JwtAuthenticationFilter(
-        JwtService jwtService,
-        UserDetailsService userDetailsService,
-        HandlerExceptionResolver handlerExceptionResolver
-    ) {
+            JwtService jwtService,
+            UserDetailsService userDetailsService,
+            HandlerExceptionResolver handlerExceptionResolver) {
         this.jwtService = jwtService;
         this.userDetailsService = userDetailsService;
         this.handlerExceptionResolver = handlerExceptionResolver;
@@ -40,10 +39,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter{
 
     @Override
     protected void doFilterInternal(
-        @NonNull HttpServletRequest request, 
-        @NonNull HttpServletResponse response, 
-        @NonNull FilterChain filterChain
-    ) throws ServletException, IOException {
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain) throws ServletException, IOException {
         final String idToken = CookieService.getCookieValue(request, "id_token");
 
         try {
@@ -52,7 +50,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter{
                 filterChain.doFilter(request, response);
                 return;
             }
-            
+
             final String email = jwtService.extractClaim(idToken, "email");
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(email);
 
@@ -64,10 +62,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter{
             }
 
             UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-                userDetails,
-                null,
-                userDetails.getAuthorities()
-            );
+                    userDetails,
+                    null,
+                    userDetails.getAuthorities());
             SecurityContextHolder.getContext().setAuthentication(authToken);
 
             filterChain.doFilter(request, response);
--- src/main/java/com/kennan/mp3player/mp3_player_api/controllers/LogoutController.java
+++ src/main/java/com/kennan/mp3player/mp3_player_api/controllers/LogoutController.java
@@ -1,23 +1,30 @@
 package com.kennan.mp3player.mp3_player_api.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kennan.mp3player.mp3_player_api.service.AuthenticationService;
 import com.kennan.mp3player.mp3_player_api.service.CookieService;
 import com.kennan.mp3player.mp3_player_api.service.JwtService;
-import com.kennan.mp3player.mp3_player_api.service.OAuthService;
-
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 public class LogoutController {
     private final AuthenticationService authenticationService;
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
 
-    public LogoutController(AuthenticationService authenticationService) {
+    public LogoutController(AuthenticationService authenticationService, JwtService jwtService,
+            UserDetailsService userDetailsService) {
         this.authenticationService = authenticationService;
+        this.jwtService = jwtService;
+        this.userDetailsService = userDetailsService;
     }
 
     @PostMapping("/logout")
@@ -25,5 +32,20 @@ public class LogoutController {
         authenticationService.terminateSession(request, response, true);
 
         return ResponseEntity.ok("Logged out successfully");
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<?> getUser(HttpServletRequest request) {
+        String idToken = CookieService.getCookieValue(request, "id_token");
+        try {
+            final String email = jwtService.extractClaim(idToken, "email");
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(email);
+            if (!jwtService.isTokenValid(idToken, userDetails)) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Token is invalid");
+            }
+            return ResponseEntity.ok(userDetails);
+        } catch (Exception error) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Token is invalid");
+        }
     }
 }
